bbd1ff99e1679c011d5f379fe5cab47a0954e0a9
Hidenori Shinohara <hidenori@stellar.org>
2021-03-15 11:13:59 -0400

    autopep8

--- executable-models/model-2/member.py
+++ executable-models/model-2/member.py
@@ -1,24 +1,28 @@
 nodeCount = 3
 
+
 def isMajority(bitmask):
     return 2 * bin(bitmask).count("1") > nodeCount
+
+
 def contains(bitmask, node):
     return ((bitmask >> node) & 1) == 1
 
+
 members = []
 for nodeId in range(nodeCount):
-    for setId in range(1<<nodeCount):
+    for setId in range(1 << nodeCount):
         if contains(setId, nodeId):
             members.append("(X=v{}&S={})".format(nodeId, setId))
 
 quorums = []
-for setId in range(1<<nodeCount):
+for setId in range(1 << nodeCount):
     if isMajority(setId):
         quorums.append("(S={})".format(setId))
 
 v_blocking_pairs = []
 for nodeId in range(nodeCount):
-    for setId in range(1<<nodeCount):
+    for setId in range(1 << nodeCount):
         if not contains(setId, nodeId) and not isMajority(setId):
             # If the set does NOT the node AND the set is NOT the majority,
             # such a set is NOT v-blocking.
@@ -34,4 +38,5 @@ template = """\
     relation is_v_blocking(X:id_t, S:set_t)
     definition is_v_blocking(X:id_t, S:set_t) = ~({})"""
 
-print(template.format("|".join(members), "|".join(quorums), "|".join(v_blocking_pairs)))
+print(template.format("|".join(members), "|".join(
+    quorums), "|".join(v_blocking_pairs)))
